2b87a493d43206941e078615573bf7ff00e523ab
Yash Sisodia <78316143+Yash-Sisodia@users.noreply.github.com>
2022-09-11 14:50:05 +0530

    While loop working

---
+++ Mybeginingofjava/src/Loopsbasic1.java
@@ -0,0 +1,14 @@
+// Print countings from 1 to 9 by While Loop
+
+import java.util.*;
+public class Loopsbasic1 {
+    public static void main(String[] args) {
+        int i = 1;
+        while(i<=9) {
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("Done");
+    }
+    
+}
