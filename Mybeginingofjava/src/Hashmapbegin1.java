45d72c6748c6465c96a65b6af499ada65e44ecab
Yash Sisodia <78316143+Yash-Sisodia@users.noreply.github.com>
2022-09-11 11:23:49 +0530

    HashMap intro

---
+++ Mybeginingofjava/src/Hashmapbegin1.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class Hashmapbegin1 {
+    public static void main(String[] args) {
+
+    //     //PUT ka behaviour
+    //     HashMap<String,Integer> hm = new HashMap<>();
+    //     hm.put("India",135);
+    //     hm.put("China",200);
+    //     hm.put("Pak",40);
+    //     hm.put("US",20);
+    //     hm.put("UK",15);
+    //     System.out.println(hm);
+
+    //     //GET ka behaviour
+        
+    //     System.out.println(hm.get("Utopia"));
+
+    //     //CONTAINS KEY
+
+    //     System.out.println(hm.containsKey("Bhutan"));
+    // 
+    HashMap<String,Integer> hm = new HashMap<>();
+    hm.put("India",135);
+    hm.put("China",200);
+
+    System.out.println(hm.containsKey("India"));
+    System.out.println(hm.containsKey("Nigeria"));
+    
+}
+    
+}
