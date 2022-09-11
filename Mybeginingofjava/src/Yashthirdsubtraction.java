9b0056fbc9b119af176f20ba7cbc2b77defce2b8
Yash Sisodia <78316143+Yash-Sisodia@users.noreply.github.com>
2022-09-11 10:17:26 +0530

    Hi

---
+++ Mybeginingofjava/src/Yashthirdsubtraction.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Yashthirdsubtraction {
+    public static void main(String [] args) {
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int sub = a - b;
+
+        System.out.println(sub);
+    }
+}
+
+
