15431936345ac1d401dff9fd1f7c6b8881727e7f
Yash Sisodia <78316143+Yash-Sisodia@users.noreply.github.com>
2022-09-11 13:21:49 +0530

    For loop

---
+++ Mybeginingofjava/src/Loopsbasic.java
@@ -0,0 +1,12 @@
+//Print Hello World 10 times by For Loop 
+
+import java.util.*;
+public class Loopsbasic {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = 10;
+    for(int i = 1 ; i<=n ; i++) {
+        System.out.println(" Hello World " +i);
+    }
+}
+}
