79d65be9446bcff744a965c800a4344f3753d56e
Yash Sisodia <78316143+Yash-Sisodia@users.noreply.github.com>
2022-09-16 23:25:04 +0530

    Day 2

--- Mybeginingofjava/src/Dayone.java
+++ Mybeginingofjava/src/Dayone.java
@@ -10,6 +10,7 @@ public class Dayone {
         for(int i = 2 ; i<n ; i++) {
             if(n%i==0) {
                 counter++;
+                System.out.println(counter);
             }
         }
         if(counter==0) {
